4719da67b258202d7bbac20e82e41bedaa487a97
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-07-14 17:25:14 -0300

    Componente MovieCard

--- movies_lib/src/App.jsx
+++ movies_lib/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h2>Movies lib</h2>
       <Outlet />
     </div>
   )
--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import MovieCard from '../components/MovieCard';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
+    const [topMovies, setTopMovies] = useState([]);
+
+    const getTopRatedMovies = async(url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setTopMovies(data.results);
+    }
+
+    useEffect(() => {
+        const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+        getTopRatedMovies(topRatedUrl);
+    }, []);
+
     return (
-        <div>Home</div>
+        <div className='container'>
+            <h2 className="title">Melhores filmes</h2>
+            <div className="movies-container">
+                {topMovies === 0 && <p>Carregando lista de filmes</p>}
+                {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
+        </div>
     )
 }
 
