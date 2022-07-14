9907bd1ecd4c297c5620ce2fa78101203a4f97ee
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-07-14 16:25:30 -0300

    Componente Navbar

---
+++ movies_lib/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+
+const Navbar = () => {
+    return (
+        <nav id="navbar">
+            <h2>
+                <Link to='/'><BiCameraMovie />MoviesLib</Link>
+            </h2>
+            <form>
+                <input type="text" placeholder='busque um filme' />
+                <button type="submit"><BiSearchAlt2 /></button>
+            </form>
+        </nav>
+    )
+}
+
+export default Navbar
